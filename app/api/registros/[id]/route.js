bac0beec0c4f9e1fc3bfa2a4d5b650a3e866acae
palominolilith-lgtm <palominolilith@gmail.com>
2026-09-17 02:00:16 -0500

    Update route.js

--- app/api/registros/[id]/route.js
+++ app/api/registros/[id]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { updateRecord, deleteRecord } from "../../../../../lib/store";
+import { updateRecord, deleteRecord } from "../../../../lib/store";
 
 export async function PUT(req,{params}) {
   const body = await req.json();
